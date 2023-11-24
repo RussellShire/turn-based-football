9afb40f633aa22d3a08c5133e22db0576b40992e
russellshire <russell.shire@gmail.com>
2023-11-24 21:08:02 +0000

    Add tailwind and build simple grid

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-auto border-2">
+    <div v-for="(boxi, indexYaxis) in 5" class="flex flex-row justify-center font-bold">
+      <div v-for="(boxii, indexXaxis) in 3" :id="(indexXaxisi+1)+'-'+(indexYaxis+1)" class="border p-5">{{indexXaxis+1}}-{{indexYaxis+1}}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/*  */
+</style>
