1bb2b72bbbd7e41494c098a6d45f57e5b9b49e9e
russellshire <russell.shire@gmail.com>
2023-11-27 19:37:04 +0000

    Add keys to v-for elements

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,17 @@
 
 <template>
   <div class="flex flex-col-reverse w-auto border-2">
-    <div v-for="(row, indexYaxis) in 10" class="flex flex-row justify-center">
+    <div 
+      v-for="(row, indexYaxis) in 10"
+      :key="indexYaxis"
+      class="flex flex-row justify-center" 
+    >
       <div 
         v-for="(box, indexXaxis) in 5" 
-        :id="(indexXaxisi+1)+'-'+(indexYaxis+1)" 
-        class="border border-black w-20 h-20 pl-1"
-        :class="((indexXaxis+1) + (indexYaxis+1)) % 2 === 0 ? 'bg-green-700' : 'bg-green-900'"  
+        :id="(indexXaxisi+1)+'-'+(indexYaxis+1)"
+        :key="indexXaxis"
+        :class="((indexXaxis+1) + (indexYaxis+1)) % 2 === 0 ? 'bg-green-700' : 'bg-green-900'"
+        class="border border-black w-20 h-20 pl-1"  
       >
         {{indexXaxis+1}}-{{indexYaxis+1}}
       </div>
